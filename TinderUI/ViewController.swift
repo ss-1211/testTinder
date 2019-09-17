4391d0f2db9a3d2552c5cdf1a2afaae7c92d69a7
SS201801 <ss12111991@yahoo.co.jp>
2019-09-17 22:28:35 +0900

    Initial Commit

---
+++ TinderUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderUI
+//
+//  Created by 佐々木駿 on 2019/09/17.
+//  Copyright © 2019 shun.sasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
